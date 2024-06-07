e54dae3b89689079de8620152d0cf7f81b3ae421
joffei2 <joffei112@gmail.com>
2024-06-07 16:54:47 +0000

    ..

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -6,13 +6,20 @@
  *
  * Return: Number of leaves in the tree, 0 if tree is NULL
  */
+
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t leave = 0, x = 0, y = 0;
+
 	if (tree == NULL)
+	{
 		return (0);
-
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-
-	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	}
+	else
+	{
+		x = binary_tree_leaves(tree->left);
+		y = binary_tree_leaves(tree->right);
+		leave = x + y;
+		return ((!x && !y) ? leaf + 1 : leaf + 0);
+	}
 }
